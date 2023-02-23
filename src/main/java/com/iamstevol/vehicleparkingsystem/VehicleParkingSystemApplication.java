018dc5de79b8f526ff13e5ef2033114a8070363a
iamstevol <stevol2015@gmail.com>
2023-02-23 19:54:13 +0100

    Vehicle Parking Management System

---
+++ src/main/java/com/iamstevol/vehicleparkingsystem/VehicleParkingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iamstevol.vehicleparkingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleParkingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleParkingSystemApplication.class, args);
+    }
+
+}
